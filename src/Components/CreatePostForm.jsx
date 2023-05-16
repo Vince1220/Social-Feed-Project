266fe09976d1b123372a1fe1dcfba558d33156ae
Vince1220 <guitarvince025@yahoo.com>
2023-05-16 14:58:57 -0400

    Create Post Logic

---
+++ src/Components/CreatePostForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const CreatePostForm = (props) => {
+    const [name, setName] = useState('');     
+    const [post, setPost] = useState('');   
+
+    function handleSubmit(formEvent){
+        formEvent.preventDefault();  
+        let newPost = {      
+            name: name,
+            post: post
+        };
+        console.log(newPost);     
+        props.addNewPost(newPost)  
+    }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <div className='border-box'>
+                <div className='form-group row'>
+                    <label htmlFor='input name' className='col-sm-1 col-form-label'> Name</label>
+                    <div className='col-sm-10'>                
+                        <input type='text' onChange={(event) => setName(event.target.value)} value={name}/>    
+                    </div>
+                </div>
+                <div className='form-group row'>
+                    <label htmlFor='input post' className='col-sm-1 col-form-label'>Post</label>
+                <div className='col-sm-10'>
+                    <input type='text' onChange={(event) => setPost(event.target.value)} value={post}/>    {/*  sending data to hook on line 6 */}
+                </div>
+                    </div>
+                <div className="form-group row">
+                    <div className="col-sm-10">
+                        <button type="submit" className="btn btn-primary">Make Post</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+     );
+}
+ 
+export default CreatePostForm;
---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Post = (props) =>{
+    const [typeOfLike, setTypeOfLike]= useState('up');
+    function handleLike(event) {
+        if (typeOfLike === 'down') {
+            setTypeOfLike('up')
+        }
+        else{
+             setTypeOfLike('down')
+        }
+
+    }
+
+    return ( 
+        <tr>
+            <td>{props.parentPost.name}</td>
+            <td>{props.parentPost.post}</td>   
+            <td><button type='react' className={`fa fa-thumbs-${typeOfLike}`} onClick={handleLike}></button></td>
+        </tr>
+         );
+    }
+     
+    export default Post;
